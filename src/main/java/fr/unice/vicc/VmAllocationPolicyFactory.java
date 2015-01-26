074def548f7658cb6f9b9910e585a6b1dec41bc0
mourjo <sen.mourjo@etu.unice.fr>
2015-01-26 12:20:07 +0100

    AntiAffinity sched

--- src/main/java/fr/unice/vicc/VmAllocationPolicyFactory.java
+++ src/main/java/fr/unice/vicc/VmAllocationPolicyFactory.java
@@ -13,6 +13,7 @@ public class VmAllocationPolicyFactory {
     VmAllocationPolicy make(String id, List<PowerHost> hosts) {
         switch (id) {
             case "naive":  return new NaiveVmAllocationPolicy(hosts);
+            case "antiAffinity" : return new AntiAffinityVmAllocationPolicy(hosts);
         }
         throw new IllegalArgumentException("No such policy '" + id + "'");
     }
